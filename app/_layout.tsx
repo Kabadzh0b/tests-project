74fd6fc39888cff16095873f62c0bb4753c3fc37
Maksym Chub <chubbudda@gmail.com>
2024-12-26 21:53:43 +0200

    feat: edit/add modal, local storage sync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Text } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{ headerRight: () => <Text>Right</Text> }}>
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="players" options={{ title: "Players" }} />
       <Tabs.Screen name="index" options={{ title: "Games" }} />
     </Tabs>
--- app/players.tsx
+++ app/players.tsx
@@ -1,62 +1,159 @@
-import React from "react";
-import { View, Text, ScrollView, StyleSheet, Dimensions } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+  Modal,
+} from "react-native";
+import {
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+} from "react-native-gesture-handler";
 
-const screenWidth = Dimensions.get("window").width;
+const headerMetrics = ["Deposit", "Withdraw", "Summary"];
+// const playerData = [
+//   { id: "1", name: "Player 1", metrics: [100, 50, 150, 200, 300] },
+//   { id: "2", name: "Player 2", metrics: [200, 75, 275, 150, 250] },
+//   { id: "3", name: "Player 3", metrics: [150, 60, 210, 100, 180] },
+// ];
 
-const headerMetrics = [
-  "Deposit",
-  "Withdraw",
-  "Summary",
-  "Metric 1",
-  "Metric 2",
-];
-const playerData = [
-  { id: "1", name: "Player 1", metrics: [100, 50, 150, 200, 300] },
-  { id: "2", name: "Player 2", metrics: [200, 75, 275, 150, 250] },
-  { id: "3", name: "Player 3", metrics: [150, 60, 210, 100, 180] },
-];
+type Player = {
+  id: string;
+  name: string;
+  deposit: number;
+  withdraw: number;
+};
 
 const PlayersMetricsTable = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const players: Player[] = JSON.parse(localStorage.getItem("players") || "[]");
   return (
-    <View style={styles.container}>
-      <ScrollView horizontal>
-        <View>
-          {/* Header Row */}
-          <View style={styles.row}>
-            <View style={[styles.fixedColumn, styles.headerCell]}>
-              <Text style={styles.headerText}>Player Name</Text>
-            </View>
-            {headerMetrics.map((metric, index) => (
-              <View key={index} style={styles.headerCell}>
-                <Text style={styles.headerText}>{metric}</Text>
-              </View>
-            ))}
-            <View style={[styles.fixedColumn, styles.headerCell]} />
+    <ScrollView horizontal style={styles.container}>
+      <View style={styles.container}>
+        {/* Header Row */}
+        <View style={styles.row}>
+          <View style={[styles.headerCell, styles.headerCell]}>
+            <Text style={styles.headerText}>Player Name</Text>
           </View>
-
-          {/* Players Data */}
-          <ScrollView>
-            {playerData.map((player) => (
-              <View key={player.id} style={styles.row}>
-                {/* Fixed Player Name Column */}
-                <View style={[styles.fixedColumn, styles.playerNameCell]}>
-                  <Text style={styles.playerNameText}>{player.name}</Text>
-                </View>
-                {/* Player Metrics */}
-                {player.metrics.map((metric, index) => (
-                  <View key={index} style={styles.metricCell}>
-                    <Text style={styles.metricText}>{metric}</Text>
-                  </View>
-                ))}
-                <View style={[styles.fixedColumn, styles.metricCell]}>
-                  <Text style={styles.metricText}>Edit</Text>
-                </View>
-              </View>
-            ))}
-          </ScrollView>
+          {headerMetrics.map((metric, index) => (
+            <View key={index} style={styles.headerCell}>
+              <Text style={styles.headerText}>{metric}</Text>
+            </View>
+          ))}
+          <Pressable
+            style={[
+              styles.headerCell,
+              {
+                backgroundColor: "#99FF99",
+              },
+            ]}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.headerText}>Create new</Text>
+          </Pressable>
         </View>
-      </ScrollView>
-    </View>
+
+        {/* Players Data */}
+        {players.map((player) => (
+          <View key={player.id} style={styles.row}>
+            {/* Fixed Player Name Column */}
+            <View style={[styles.headerCell, styles.playerNameCell]}>
+              <Text style={styles.playerNameText}>{player.name}</Text>
+            </View>
+            {/* Player Metrics */}
+            <View style={styles.metricCell}>
+              <Text style={styles.metricText}>{player.deposit}</Text>
+            </View>
+            <View style={styles.metricCell}>
+              <Text style={styles.metricText}>{player.withdraw}</Text>
+            </View>
+            <View style={styles.metricCell}>
+              <Text style={styles.metricText}>
+                {player.withdraw - player.deposit}
+              </Text>
+            </View>
+            <Pressable
+              style={styles.headerCell}
+              onPress={() => setModalVisible(true)}
+            >
+              <Text style={styles.metricText}>Edit</Text>
+            </Pressable>
+          </View>
+        ))}
+      </View>
+      <Modal visible={modalVisible} transparent animationType="slide">
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: 20,
+              backgroundColor: "#fff",
+              borderRadius: 10,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.25,
+              shadowRadius: 4,
+              elevation: 5,
+              gap: 30,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              Edit Player
+            </Text>
+            <View style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+              <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                Player Name
+              </Text>
+              <TextInput
+                style={{ fontSize: 16, fontWeight: "bold" }}
+                placeholder="Enter player name"
+              />
+            </View>
+            <View style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+              <Pressable
+                style={{
+                  backgroundColor: "#000",
+                  padding: 10,
+                  borderRadius: 5,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={{ color: "#fff", fontSize: 16 }}>Save</Text>
+              </Pressable>
+              <Pressable
+                style={{
+                  backgroundColor: "#000",
+                  padding: 10,
+                  borderRadius: 5,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{ color: "#fff", fontSize: 16 }}
+                  onPress={() => setModalVisible(false)}
+                >
+                  Cancel
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </ScrollView>
   );
 };
 
@@ -64,21 +161,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    display: "flex",
+    flexDirection: "column",
   },
   row: {
     flexDirection: "row",
     alignItems: "center",
   },
-  fixedColumn: {
-    width: 100,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f7f7f7",
-    borderRightWidth: 1,
-    borderRightColor: "#ccc",
-  },
   headerCell: {
-    width: 100,
+    flex: 1,
+    minWidth: 100,
     height: "100%",
     display: "flex",
     justifyContent: "center",
@@ -94,7 +186,8 @@ const styles = StyleSheet.create({
     borderBottomColor: "#ccc",
   },
   metricCell: {
-    width: 100,
+    flex: 1,
+    minWidth: 100,
     justifyContent: "center",
     alignItems: "center",
     padding: 10,
